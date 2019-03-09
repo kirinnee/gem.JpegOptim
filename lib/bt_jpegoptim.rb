5843d068e6d60b7274944a37ae10a1f80c7cbb50
kirinnee <kirinnee@gmail.com>
2019-03-09 15:45:37 +0800

    Initial Commit~

---
+++ lib/bt_jpegoptim.rb
@@ -0,0 +1,6 @@
+require "bt_jpegoptim/version"
+
+module	BtJpegoptim
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/bt_jpegoptim/version.rb
@@ -0,0 +1,3 @@
+module BtJpegoptim
+  VERSION = "0.1.0"
+end
